0ba067a13928f8291df565108f66f0f5b8f45db8
Tije Vlam <tije@2m7.nl>
2025-09-09 23:10:05 +0200

    https and dates

--- server.js
+++ server.js
@@ -564,6 +564,18 @@ async function main(){
         console.log('HTTP-server luistert op poort 8080');
     });
 
+    // also make it available on https
+
+    const httpsOptions = {
+        key: fs.readFileSync('/etc/letsencrypt/live/live.find-it.nl/privkey.pem'),
+        cert: fs.readFileSync('/etc/letsencrypt/live/live.find-it.nl/fullchain.pem')
+
+
+    https.createServer(httpsOptions, app).listen(8443, () => {
+        console.log('HTTPS-server luistert op poort 8443');
+    });
+
+
     // Start the TCP/IP Server to listen to Mylaps Exporter
     tcpServer.listen(TCP_PORT, () => {
         console.log(`TCP server listening on port ${TCP_PORT}`);
