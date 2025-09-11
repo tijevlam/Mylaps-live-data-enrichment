d2bfdc489e763ae34f7a5b2cf1c985e8837d0fe6
Tije Vlam <tije@2m7.nl>
2025-09-11 10:24:44 +0200

    Update server.js

--- server.js
+++ server.js
@@ -150,7 +150,7 @@ async function parseCsv(file) {
 
 function matchChipBibToBib(bibs, chipbib) {
     // console.log(bibs[0], chip)
-    const bib = bibs.find(bib => bib.bib == chipbib);
+    const bib = Object.values(bibs).find(bib => bib.bib === chipbib);
     // console.log(bib);
     for (const k in bib) {
         if (bib.hasOwnProperty(k) && bib[k] != null) {
